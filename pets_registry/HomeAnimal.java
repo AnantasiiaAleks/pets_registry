5aa6ecd2390f98f464126e0590129c2d9fafc316
AnastasiiaAleks <chiffka@gmail.com>
2024-07-01 00:25:15 +0300

    Added abstract class HomeAnimal

---
+++ pets_registry/HomeAnimal.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public abstract class HomeAnimal extends Animal{
+    public HomeAnimal(String name, LocalDate birthDate) {
+        super(name, birthDate);
+
+    }
+}
