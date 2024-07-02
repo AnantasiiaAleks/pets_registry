2260a0f9a37df27fc38fe4eb80246c18cd6f68f6
AnastasiiaAleks <chiffka@gmail.com>
2024-07-02 14:15:20 +0300

    create method getAnimalByTitle

--- pets_registry/Model/AnimalTitle.java
+++ pets_registry/Model/AnimalTitle.java
@@ -13,6 +13,17 @@ public enum AnimalTitle {
     AnimalTitle (String animTitle) {
         this.animTitle = animTitle;
     }
+
+    public static AnimalTitle getAnimalByTitle(String title) {
+        for (AnimalTitle anim : values()) {
+            if (anim.getTitle().equals(title)) {
+                return anim;
+            }
+        }
+
+        throw new IllegalArgumentException("No animal found with title: [" + title + "]");
+    }
+
     public String getTitle() {
         return animTitle;
     }
