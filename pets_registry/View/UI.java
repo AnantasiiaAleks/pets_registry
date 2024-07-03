b9df475f6d0f0bf5a8ecd93b4ae2805af24abe33
AnastasiiaAleks <chiffka@gmail.com>
2024-07-04 01:33:21 +0300

    finished UI

--- pets_registry/View/UI.java
+++ pets_registry/View/UI.java
@@ -14,18 +14,37 @@ public class UI {
 
     public void printMenu() {
         System.out.println("__________*** MENU ***__________");
-        System.out.println("Press 1-4 to select any command:\n" +
+        System.out.println("Press 1-5 to select any command:\n" +
                 "*1* Add new animal.\n" +
                 "*2* Search animal.\n" +
                 "*3* Show all animals.\n" +
-                "*4* Quit. ");
+                "*4* Learn new command.\n" +
+                "*5* Quit. \n" +
+                "\n" +
+                "Your choice: ");
     }
 
     public void addNewAnimal() {
-        String getTitle = InputFromUser.inputText("Please input your animals title").toLowerCase();
+        String getTitle = InputFromUser.inputText("Please input your animals title: ").toLowerCase();
         AnimalTitle animalTitle = AnimalTitle.getAnimalByTitle(getTitle);
         AnimalType animalType = AnimalType.getAnimalByItsTitle(animalTitle);
-        String name = InputFromUser.inputText("Please enter name");
-        LocalDate birthdate = InputFromUser.inputDate("Please enter birth date");
+        String name = InputFromUser.inputText("Please enter name: ");
+        LocalDate birthdate = InputFromUser.inputDate("Please enter birth date: ");
+    }
+
+    public void searchAnimal() {
+        int query = InputFromUser.inputInt("Please enter id for searching: ");
+    }
+
+    public void showAllAnimals(){
+        System.out.println("All animals: ");
+    }
+
+    public void learnNewCommand() {
+        int query = InputFromUser.inputInt("First you need to find an animal. Please enter id: ");
+        String newCommand = InputFromUser.inputText("Enter new command: ");
+    }
+    public void quit(){
+        System.out.println("Bye-bye!");
     }
 }
