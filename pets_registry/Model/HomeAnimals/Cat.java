55b38dc12779630e39a0e36d0a175f1a20dba936
AnastasiiaAleks <chiffka@gmail.com>
2024-09-03 10:28:03 +0300

    Упрощен код классов HomeAnimal и его наследников: Cat, Dog, Hamster. Для каждого установлена привязка к AnimalType и AnimalTitle

--- pets_registry/Model/HomeAnimals/Cat.java
+++ pets_registry/Model/HomeAnimals/Cat.java
@@ -1,27 +1,12 @@
 package Model.HomeAnimals;
 
 import Model.AnimalTitle;
-import Model.AnimalType;
-
 import java.time.LocalDate;
 
 public class Cat extends HomeAnimal{
 
-    public String currentTitle = AnimalTitle.CAT.getTitle();
     public Cat(String name, LocalDate birthDate) {
         super(name, birthDate);
-        AnimalType animalType = AnimalType.HOMEANIMAL;
-        AnimalTitle animalTitle = AnimalTitle.CAT;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Animal: " +
-                currentType + ", " + currentTitle +
-                ", id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", birthDate=" + getBirthDate().toString() +
-                '.';
+        setAnimalTitle(AnimalTitle.CAT);
     }
 }
