7e3211041027b21ff41272e4e865b8ad23ac31ca
AnastasiiaAleks <chiffka@gmail.com>
2024-09-03 11:22:29 +0300

    Подготовлен интерфейс для работы с БД

---
+++ pets_registry/Controller/DataBase/DataBase.java
@@ -0,0 +1,16 @@
+package Controller.DataBase;
+
+import Model.Animal;
+
+import java.util.List;
+
+public interface DataBase {
+
+    boolean addAnimal(Animal animal);
+
+    List<Animal> showAllAnimals();
+
+    Animal searchAnimalById(int animalId);
+
+    boolean deleteAnimal(int animalId);
+}
