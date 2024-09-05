bebc8db5d15ae849d85029577545f86fd53889f6
AnastasiiaAleks <chiffka@gmail.com>
2024-09-05 18:19:39 +0300

    Пересмотрены утилиты валидации вносимых данных и ввода информации от пользователя

--- pets_registry/Utilities/DataValidator.java
+++ pets_registry/Utilities/DataValidator.java
@@ -3,7 +3,7 @@ package Utilities;
 import java.text.SimpleDateFormat;
 
 public class DataValidator {
-    boolean isDateValid(String dateStr) {
+    public static boolean isDateValid(String dateStr) {
         SimpleDateFormat myFormat = new SimpleDateFormat("dd.MM.yyyy");
         myFormat.setLenient(false);
         try {
@@ -14,7 +14,7 @@ public class DataValidator {
         }
     }
 
-    public boolean isTextValid(String text){
+    public static boolean isTextValid(String text){
         if (text.length() > 1){
             for (int i = 0; i < text.length(); i++) {
                 if (text.matches("^[А-Яа-я]+$") ||
--- pets_registry/Utilities/InputFromUser.java
+++ pets_registry/Utilities/InputFromUser.java
@@ -1,11 +1,49 @@
 package Utilities;
 
+import Model.Animal;
+import Model.AnimalInfo;
+import Model.AnimalTitle;
+import Model.AnimalType;
+
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.time.LocalDate.parse;
 
 public interface InputFromUser {
+
+    static AnimalInfo parseAnimalInfoFromUser(String text) {
+        String[] animalInfo = text.split(" ");
+        String infoName = null;
+        LocalDate infoBirthDate = null;
+        AnimalTitle infoTitle = null;
+
+        if (animalInfo.length != 3) {
+            throw new IllegalArgumentException("Вы ввели неверное количество данных");
+        }
+        if (DataValidator.isTextValid(animalInfo[0])) {
+            infoName = animalInfo[0];
+        }
+        if (DataValidator.isDateValid(animalInfo[1])) {
+            infoBirthDate = LocalDate.parse(animalInfo[1]);
+        }
+        if (AnimalTitle.contains(animalInfo[2])) {
+            infoTitle = AnimalTitle.valueOf(animalInfo[2].toUpperCase());
+        }
+        AnimalType animalType = typeByTitle(animalInfo[2].toLowerCase());
+        return new AnimalInfo(animalType, infoTitle, infoName, infoBirthDate);
+    }
+
+    private static AnimalType typeByTitle(String title){
+        if (title.equals("кошка") || title.equals("собака") || title.equals("хомяк")) {
+            return AnimalType.HOMEANIMAL;
+        }
+        if (title.equals("лошадь") || title.equals("верблюд") || title.equals("осел")) {
+            return AnimalType.PACKANIMAL;
+        }
+        throw new IllegalArgumentException("Не найдено");
+    }
 
     static String inputText(String message) {
         Scanner sc = new Scanner(System.in);
@@ -19,7 +57,7 @@ public interface InputFromUser {
         return sc.nextInt();
     }
 
-    static LocalDate inputDate(String message) {
+    default LocalDate inputDate(String message) {
         Scanner sc = new Scanner(System.in);
         System.out.println(message);
         return parse(sc.nextLine());
